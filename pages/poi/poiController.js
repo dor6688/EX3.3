ede2d8d652721692f2e1bcc9d4e99320e62bbfbc
שולמית גלזר <glazersh@auth.ad.bgu.ac.il>
2019-06-29 20:46:26 +0300

    modal try

--- pages/poi/poiController.js
+++ pages/poi/poiController.js
@@ -6,7 +6,27 @@ angular.module("myApp")
         $scope.cat_name;
         $scope.empty_result;
         $scope.heart = [];
-        $scope.flag = [1,1];
+        $scope.flag = [1, 1];
+        // Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the image and insert it inside the modal - use its "alt" text as a caption
+var img = document.getElementById("myImg");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+img.onclick = function(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  captionText.innerHTML = this.alt;
+}
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() { 
+  modal.style.display = "none";
+}
         $http.get('http://localhost:3000/points/getCategories').then(function (response) {
             $scope.Categories = response.data;
         });
@@ -15,9 +35,9 @@ angular.module("myApp")
                 .then(function (response) {
                     self.Pois = response.data;
                     $scope.default_image = "https://upload.wikimedia.org/wikipedia/commons/b/b9/GJL-fft-herz.svg";
-                        for(i in self.Pois){
-                            $scope.heart[i] = $scope.default_image;
-                        }
+                    for (i in self.Pois) {
+                        $scope.heart[i] = $scope.default_image;
+                    }
                 }, function (error) {
                     window.alert("NO !")
                 })
@@ -28,9 +48,9 @@ angular.module("myApp")
                     self.Pois = response.data;
                     if (self.Pois.length == 0) {
                         window.alert("Sorry, didn't found anything... ")
-                    }else{
+                    } else {
                         $scope.default_image = "https://upload.wikimedia.org/wikipedia/commons/b/b9/GJL-fft-herz.svg";
-                        for(i in self.Pois){
+                        for (i in self.Pois) {
                             $scope.heart[i] = $scope.default_image;
                         }
                     }
@@ -44,18 +64,13 @@ angular.module("myApp")
             }
             $http.put('http://localhost:3000/points/addPoiWatching', poi_name_jason)
                 .then(function (response) {
-                    poi_watch2 = response.data[0].poiWatching;
-                    var newWin = open('url', 'windowName', 'height=300,width=300');
-                    full_description = "Poi Watching : " + poi_watch2 + "<br>" +
-                        "Poi description : " + poi_desc + "<br>" +
-                        "Poi rate : " + poi_rate
 
-                    newWin.document.write(full_description);
 
                 }, function (error) {
 
                 })
         }
+
 
 
         $scope.like_poi = function (poi_name, i) {
@@ -64,19 +79,23 @@ angular.module("myApp")
                 $scope.heart[i] = "https://www.warrenstore.com/wp-content/uploads/2015/06/clipart-heart-LiKzza9ia.png"
                 $scope.flag[i] = 2;
                 $http({
-                    url:'http://127.0.0.1:3000/privateUser/addFavoritePoi',
+                    url: 'http://127.0.0.1:3000/privateUser/addFavoritePoi',
                     method: "put",
                     headers: {
                         'Content-Type': 'application/json',
                         'x-auth-token': $rootScope.userToken
                     },
-                    data : data
+                    data: data
                 })
             }
             else {
                 $scope.heart[i] = "https://upload.wikimedia.org/wikipedia/commons/b/b9/GJL-fft-herz.svg"
                 $scope.flag[i] = 1;
             }
+        }
+
+        $scope.AddReview = function (str){
+
         }
 
 
